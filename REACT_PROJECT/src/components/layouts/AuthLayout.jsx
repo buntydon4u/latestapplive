5b6e5d4b391dc9acf1b887b60b13fa04a25c44ed
amitisdeveloper <amitisdeveloper@gmail.com>
2026-05-25 15:37:12 +0530

    done

--- REACT_PROJECT/src/components/layouts/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom'
-
-export default function AuthLayout() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
-      <Outlet />
-    </div>
-  )
-}
